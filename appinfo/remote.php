ea2d7d3170252e79f9718362d802ed1485f7f683
Robin Appelman <icewind@owncloud.com>
2014-07-28 15:10:09 +0200

    fix caldav when the calendar app is only enabled for specific groups

--- appinfo/remote.php
+++ appinfo/remote.php
@@ -5,23 +5,23 @@
  * later.
  * See the COPYING-README file.
  */
-OCP\App::checkAppEnabled('calendar');
+OCP\App::checkAppEnabled('calendar', true);
 
-if(substr(OCP\Util::getRequestUri(),0,strlen(OC_App::getAppWebPath('calendar').'/caldav.php')) == OC_App::getAppWebPath('calendar'). '/caldav.php') {
-	$baseuri = OC_App::getAppWebPath('calendar').'/caldav.php';
+if (substr(OCP\Util::getRequestUri(), 0, strlen(OC_App::getAppWebPath('calendar') . '/caldav.php')) == OC_App::getAppWebPath('calendar') . '/caldav.php') {
+	$baseuri = OC_App::getAppWebPath('calendar') . '/caldav.php';
 }
 
 // only need authentication apps
-$RUNTIME_APPTYPES=array('authentication');
+$RUNTIME_APPTYPES = array('authentication');
 OC_App::loadApps($RUNTIME_APPTYPES);
-if(\OCP\App::isEnabled('contacts')) {
+if (\OCP\App::isEnabled('contacts', true)) {
 	\OCP\Share::registerBackend('addressbook', 'OCA\Contacts\Share\Addressbook', 'contact');
 }
 
 // Backends
 $authBackend = new OC_Connector_Sabre_Auth();
 $principalBackend = new OC_Connector_Sabre_Principal();
-$caldavBackend    = new OC_Connector_Sabre_CalDAV();
+$caldavBackend = new OC_Connector_Sabre_CalDAV();
 $requestBackend = new OC_Connector_Sabre_Request();
 
 // Root nodes
@@ -34,18 +34,24 @@ $calendarRoot->disableListing = true; // Disable listening
 $nodes = array(
 	$Sabre_CalDAV_Principal_Collection,
 	$calendarRoot,
-	);
+);
 
 // Fire up server
 $server = new \Sabre\DAV\Server($nodes);
 $server->httpRequest = $requestBackend;
 $server->setBaseUri($baseuri);
 // Add plugins
-$server->addPlugin(new \Sabre\DAV\Auth\Plugin($authBackend,'ownCloud'));
+$server->addPlugin(new \Sabre\DAV\Auth\Plugin($authBackend, 'ownCloud'));
 $server->addPlugin(new \Sabre\CalDAV\Plugin());
 $server->addPlugin(new \Sabre\DAVACL\Plugin());
 $server->addPlugin(new \Sabre\DAV\Browser\Plugin(false)); // Show something in the Browser, but no upload
 $server->addPlugin(new \Sabre\CalDAV\ICSExportPlugin());
+$server->addPlugin(new \OC\Connector\Sabre\AppEnabledPlugin(
+	'calendar',
+	OC::$server->getUserSession(),
+	OC::$server->getConfig(),
+	OC::$server->getGroupManager()
+));
 
 // And off we go!
 $server->exec();
